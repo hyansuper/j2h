e4fcf108d3fa3ecaee96713054ce93934fc0b26a
hyan <hyansuper@foxmail.com>
2023-12-28 21:52:33 +0800

    Update j2h.js

--- j2h.js
+++ j2h.js
@@ -4,18 +4,18 @@
 	var binary_attr = ['selected', 'checked'];
 	var special_prop=['html','{}','<>','[]'];
 	var get=(o,attr,fallback)=>o.hasOwnProperty(attr)?o[attr]:fallback;
+	var if_fn=(f,...d)=>typeof(f)=='function'?f(...d):f;
 	var j2h_fn=(tmpl,data,index)=>{
 		if(Array.isArray(tmpl))return tmpl.map(t=>j2h_fn(t,data)).join('');
 		if(typeof(tmpl)=='object'){
 			var ndata=get(tmpl,'{}',o=>o)(data); // new data
-			var idata=get(tmpl,'[]',o=>ndata)(data); // inner data
+			var idata=get(tmpl,'[]',o=>ndata)(data); // inner data for inner html
 			var html=get(tmpl,'html','');
-			var tag=get(tmpl,'<>','div');
-			var attr_list=Object.entries(tmpl).filter(([k,v])=>!special_prop.includes(k)).map(([k,v])=>{if(typeof(v)=='function')v=v(ndata,index);return binary_attr.includes(k)?(v?k:''):`${k}='${v}'`}).join('');
+			var tag=if_fn(get(tmpl,'<>','div'),ndata);
+			var attr_list=Object.entries(tmpl).filter(([k,v])=>!special_prop.includes(k)).map(([k,v])=>{v=if_fn(v,ndata,index);return binary_attr.includes(k)?(v?k:''):`${k}='${v}'`}).join(' ');
 			return '<' + tag + (attr_list.length? (' '+attr_list): '') + (no_tail_el.includes(tag)? '/>': `>${j2h_fn(html,idata,index)}</${tag}>`);
 		}
-		if(typeof(tmpl)=='function') return ''+tmpl(data,index);
-		return ''+tmpl;
+		return ''+if_fn(tmpl,data,index);
 	};
 	j2h=tmpl=>{
 		var fn=data=>j2h_fn(tmpl,data);
